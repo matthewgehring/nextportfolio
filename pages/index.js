c944db2312b1fbd4b0c459b5d3ead7364981ae7b
matthewgehring <mgehring9@gmail.com>
2023-09-11 22:27:18 -0700

    square layout

--- pages/index.js
+++ pages/index.js
@@ -20,22 +20,28 @@ const PortfolioPage = () => {
 
   const layoutDict = {
     about:[
-      { i: 'intro', x: 0, y: 0, w: 3, h: 1 },
-      { i: 'project1', x: 6, y: 0, w: 2, h: 3 },
-      { i: 'project2', x: 0, y: 2, w: 2, h: 3 },
-      { i: 'project3', x: 6, y: 2, w: 2, h: 3 },
-      { i: 'darkModeToggle', x: 0, y: 4, w: 1, h: 1 },
-      { i: 'contact', x: 3, y: 4, w: 3, h: 1 },
-      { i: 'socialMedia', x: 6, y: 4, w: 3, h: 1 },
+      { i: 'intro', x: 0, y: 0, w: 4, h: 1 },
+      { i: 'map', x: 3, y: 0, w: 2, h: 1},
+      { i: 'contact', x: 3, y: 2, w: 4, h: 1},
+      { i: 'blog', x: 3, y: 4,w: 4, h: 1 },
+      { i: 'project1', x: 6, y: 0, w: 2, h: 2 },
+      { i: 'project2', x: 0, y: 2, w: 2, h: 2 },
+      { i: 'project3', x: 6, y: 2,   w: 4, h: 1},
+      { i: 'darkModeToggle', x: 0, y: 4, w: 2, h: 1},
+      { i: 'spotify', x: 0, y: 4, w: 2, h: 1 },
+      { i: 'socialMedia', x: 6, y: 4, w: 2, h: 1 },
     ],
     projects:[
-      { i: 'intro', x: 0, y: 5, w: 3, h: 1 },
-      { i: 'project1', x: 0, y: 0, w: 2, h: 3 },
-      { i: 'project2', x: 3, y: 0, w: 2, h: 3 },
-      { i: 'project3', x: 3, y: 0, w: 2, h: 3 },
-      { i: 'darkModeToggle', x: 0, y: 4, w: 1, h: 1 },
-      { i: 'contact', x: 5, y: 4, w: 3, h: 1 },
-      { i: 'socialMedia', x: 6, y: 4, w: 3, h: 1 },
+      { i: 'intro', x: 3, y: 2, w: 5, h: 2},
+      { i: 'contact', x: 0, y: 6, w: 5, h: 2 },
+      { i: 'map', x: 3, y: 3, w: 1, h: 1 },
+      { i: 'blog', x: 3, y: 4, w: 5, h: 2 },
+      { i: 'project1', x: 0, y: 0, w: 2.5, h: 5 },
+      { i: 'project2', x: 3, y: 0, w: 2.5, h: 5 },
+      { i: 'project3', x: 6, y: 0,  w: 5, h: 2 },
+      { i: 'darkModeToggle', x: 0, y: 6, w: 2.5, h: 2},
+      { i: 'spotify', x: 0, y: 6, w: 2.5, h: 2 },
+      { i: 'socialMedia', x: 6, y: 6, w: 2.5, h: 2 },
     ]
   }
 
@@ -44,47 +50,72 @@ const PortfolioPage = () => {
   };
 
   return (
-    <div className={styles.grid}>
     <div className={darkMode ? styles.darkMode : styles.lightMode}>
     <div className={styles.navbar}>
-        <div>Tu nombre</div>
+        <div>MG</div>
         <div className={styles.navButtons}>
           <button onClick={() => handleButtonClick('about')}>About</button>
           <button onClick={() => handleButtonClick('projects')}>Projects</button>
           <button onClick={() => handleButtonClick('contact')}>Contact</button>
         </div>
       </div>
-      <ResponsiveGridLayout className="layout" layouts={layouts} isResizable={false} cols={{ lg: 6, md: 10, sm: 6, xs: 4, xxs: 2 }}>
-        <div className={styles.card} key="intro">
-          <h2>Intro</h2>
-          <p>Welcome to my portfolio!</p>
+    <div className={styles.gridPage}>
+      <div className={styles.gridContainer}>
+          <ResponsiveGridLayout 
+            className="layout" 
+            layouts={layouts} 
+            compactType='vertical'
+            isResizable={false}
+            transformScale
+            cols={{ lg: 20, md:8  }}
+            margin={[16,16]}
+            containerPadding={[0,0]}
+            rowHeight={280}
+            maxRows={12}
+            >
+            <div className={styles.card} key="intro">
+              <h2>Intro</h2>
+              <p>Welcome to my portfolio!</p>
+            </div>
+            <div className={styles.card} key="project1">
+              <h2>Project 1</h2>
+              <p>Description of project 1.</p>
+            </div>
+            <div className={styles.card} key="project2">
+              <h2>Project 2</h2>
+              <p>Description of project 2.</p>
+            </div>
+            <div className={styles.card} key="project3">
+              <h2>Project 3</h2>
+              <p>Description of project 3.</p>
+            </div>
+            <div className={styles.card} key="darkModeToggle">
+              <h2>Dark Mode</h2>
+              <button onClick={toggleDarkMode}>Toggle</button>
+            </div>
+            <div className={styles.card} key="contact">
+              <h2>Contact</h2>
+              <p>Email: example@example.com</p>
+            </div>
+            <div className={styles.card} key="socialMedia">
+              <h2>Social Media</h2>
+              <p>Links to social media.</p>
+            </div>
+            <div className={styles.card} key="blog">
+              <h2>Blog</h2>
+              <p>Links to blog.</p>
+            </div>
+            <div className={styles.card} key="map">
+              <h2>map</h2>
+              <p>map</p>
+            </div>
+            <div className={styles.card} key="spotify">
+              <h2>Spotify</h2>
+              <p>Spotify.</p>
+            </div>
+          </ResponsiveGridLayout>
         </div>
-        <div className={styles.card} key="project1">
-          <h2>Project 1</h2>
-          <p>Description of project 1.</p>
-        </div>
-        <div className={styles.card} key="project2">
-          <h2>Project 2</h2>
-          <p>Description of project 2.</p>
-        </div>
-        <div className={styles.card} key="project3">
-          <h2>Project 3</h2>
-          <p>Description of project 3.</p>
-        </div>
-        <div className={styles.card} key="darkModeToggle">
-          <h2>Dark Mode</h2>
-          <button onClick={toggleDarkMode}>Toggle</button>
-        </div>
-        <div className={styles.card} key="contact">
-          <h2>Contact</h2>
-          <p>Email: example@example.com</p>
-        </div>
-        <div className={styles.card} key="socialMedia">
-          <h2>Social Media</h2>
-          <p>Links to social media.</p>
-        </div>
-      </ResponsiveGridLayout>
-    </div>
+      </div>
     </div>
   );
 };
